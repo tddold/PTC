a1387bc1c950d2f3daa76c1b875249bb42dd1037
tddold <tdd@dekom.bg>
2018-09-10 02:21:32 +0300

    First commit

---
+++ Source/Web/PTC.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PTC.Web.MvcApplication" Language="C#" %>
---
+++ Source/Web/PTC.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PTC.Web.Startup))]
+namespace PTC.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
