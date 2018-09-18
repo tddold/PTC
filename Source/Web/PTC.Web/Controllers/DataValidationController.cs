44085b2fbd53d2e0e4caf3d883abc1593afb200e
Todor Dakov <tdd@dekom.bg>
2018-09-18 19:48:26 +0300

    Update

---
+++ Source/Web/PTC.Web/Controllers/DataValidationController.cs
@@ -0,0 +1,31 @@
+﻿using PTC.Web.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PTC.Web.Controllers
+{
+    public class DataValidationController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SavePerson(PersonViewModel person)
+        {
+            if (ModelState.IsValid)
+            {
+                // sve to db
+                TempData["Success"] = "You saved the person";
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(nameof(Index), person);
+        }
+    }
+}
--- Source/Web/PTC.Web/ViewModels/PersonViewModel.cs
+++ Source/Web/PTC.Web/ViewModels/PersonViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,11 @@ namespace PTC.Web.ViewModels
 {
     public class PersonViewModel
     {
+        [Required]
+        [StringLength(100, MinimumLength = 10)]
         public string Name { get; set; }
 
+        [Range(18, 100)]
         public int Age { get; set; }
 
         public bool IsActiv { get; set; }
